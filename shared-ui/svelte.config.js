d4f1607318a3fa8f5c090d4987caeb7379e1ada0
Tiziano Santoro <tzn@google.com>
2025-09-06 22:32:42 -0400

    Fix svelte config

---
+++ shared-ui/svelte.config.js
@@ -0,0 +1,17 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    files: {
+      lib: 'src',
+    },
+    package: {
+      dir: 'dist',
+      emitTypes: true,
+    }
+  }
+};
+
+export default config;
