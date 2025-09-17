59553609bf80b478ba2b9046240607b9c848661b
Tiziano Santoro <tzn@google.com>
2025-09-17 12:37:33 +0100

    Remove unused file

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,4 +1,3 @@
 // Note: './types' contains only TypeScript interfaces and is not exported at runtime
 export * from './StructuralEditor.js';
-// export * from './schema-utils.js'; // Temporarily disabled due to CommonJS/ES module conflicts
 export * from './forma-parser.js';
